8dd1aca055137da7c3483d971001de6ca305843b
abi01384 <ryohei.suganuma3@askul.com>
2021-10-03 18:32:41 +0900

    first_commit

---
+++ src/main/kotlin/cypher/springbootcamp/SpringBootcampApplication.kt
@@ -0,0 +1,11 @@
+package cypher.springbootcamp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootcampApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootcampApplication>(*args)
+}
---
+++ src/test/kotlin/cypher/springbootcamp/SpringBootcampApplicationTests.kt
@@ -0,0 +1,13 @@
+package cypher.springbootcamp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootcampApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
